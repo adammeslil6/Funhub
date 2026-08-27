3bd70c2d922e15a6479d10c0e044fe4a238633b2
adammeslil6 <adammeslil6@gmail.com>
2026-08-27 23:04:40 +0200

    Implement script key validation
    Add key validation to ensure only authorized access.

--- Funhub.lua
+++ Funhub.lua
@@ -1,4 +1,11 @@
-local function _try(fn, ...) local ok, err = pcall(fn, ...); return ok, err end
+local validKey = "ADAM1234"
+
+if not script_key or script_key ~= validKey then
+    warn("❌ Invalid key")
+    return
+end
+
+print("✅ Key valide")local function _try(fn, ...) local ok, err = pcall(fn, ...); return ok, err end
 
 _try(function() if setthreadidentity then setthreadidentity(8) end end)
 _try(function() if setidentity then setidentity(8) end end)
